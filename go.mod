ba5e80f0486862f568b444c653621a699608cc62
toklove <superz2586@gmail.com>
2023-12-25 20:00:02 +0800

    commit

--- go.mod
+++ go.mod
@@ -2,21 +2,34 @@ module marketData
 
 go 1.20
 
-require github.com/polygon-io/client-go v1.16.2
+require (
+	github.com/gofiber/contrib/websocket v1.3.0
+	github.com/gofiber/fiber/v2 v2.51.0
+	github.com/polygon-io/client-go v1.16.2
+	github.com/sirupsen/logrus v1.9.3
+	go.mongodb.org/mongo-driver v1.13.1
+	gorm.io/driver/mysql v1.5.2
+	gorm.io/gorm v1.25.5
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.6 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/fasthttp/websocket v1.5.7 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/form/v4 v4.2.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.16.0 // indirect
+	github.com/go-resty/resty/v2 v2.10.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/gofiber/contrib/websocket v1.3.0 // indirect
-	github.com/gofiber/fiber/v2 v2.51.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
@@ -30,14 +43,11 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.13.1 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/exp v0.0.0-20220414153411-bcd21879b8fd // indirect
 	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637 // indirect
-	gorm.io/driver/mysql v1.5.2 // indirect
-	gorm.io/gorm v1.25.5 // indirect
 )
--- main.go
+++ main.go
@@ -5,23 +5,25 @@ import (
 	"fmt"
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
+	polygonRest "github.com/polygon-io/client-go/rest"
+	restModels "github.com/polygon-io/client-go/rest/models"
 	polygonws "github.com/polygon-io/client-go/websocket"
 	"github.com/polygon-io/client-go/websocket/models"
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
+	"net/http"
 	"os"
 	"time"
 )
 
 var (
-	client     *mongo.Client
-	err        error
-	db         *mongo.Database
-	collection *mongo.Collection
-	mysqlDb    *gorm.DB
+	client  *mongo.Client
+	err     error
+	db      *mongo.Database
+	mysqlDb *gorm.DB
 )
 
 type MarketCategory struct {
@@ -46,10 +48,14 @@ type marketData struct {
 
 func main() {
 
-	os.Setenv("POLYGON_API_KEY", "RtG8Z5y9vQEeBIAKWpGbfIUSGJiXbGEg")
+	os.Setenv("POLYGON_API_KEY", "KsgzThK2sJ2GY3NVGyt2GSsKuWRpoSPp")
 
 	//用fastHttp启动websocket服务
 	app := fiber.New()
+
+	logger := logrus.New()
+	logger.SetLevel(logrus.DebugLevel)
+	logger.SetFormatter(&logrus.JSONFormatter{})
 
 	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
 		// Websocket logic
@@ -70,21 +76,30 @@ func main() {
 				}
 			}()
 
-			log.Printf("Read: %s", msg)
+			logger.Printf("Read: %s", msg)
 
 			err = c.WriteMessage(mtype, msg)
 			if err != nil {
 				break
 			}
 		}
-		log.Println("Error:", err)
+		logger.Println("Error:", err)
 	}))
 
 	//1.建立连接
 	if client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017").SetConnectTimeout(5*time.Second)); err != nil {
-		fmt.Print(err)
+		fmt.Println("连接失败")
+		fmt.Print(err.Error())
 		return
 	}
+
+	// 检查连接
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 
 	//2.选择数据库 marketData
 	db = client.Database("marketData")
@@ -102,7 +117,6 @@ func main() {
 	mysqlDb.Table("market_categories").Find(&marketCategory)
 
 	for _, category := range marketCategory {
-
 		market := polygonws.Stocks
 		topic := polygonws.StocksSecAggs
 
@@ -111,87 +125,157 @@ func main() {
 			topic = polygonws.CryptoSecAggs
 		} else if category.Name == "Forex" {
 			market = polygonws.Forex
-			topic = polygonws.ForexSecAggs
+			topic = polygonws.ForexMinAggs
 		} else if category.Name == "Indices" {
 			market = polygonws.Indices
 		}
-
-		// create a new client
+		//create a new client
 		c, err := polygonws.New(polygonws.Config{
-			APIKey: os.Getenv("POLYGON_API_KEY"),
+			APIKey: "KsgzThK2sJ2GY3NVGyt2GSsKuWRpoSPp",
 			Feed:   polygonws.RealTime,
 			Market: market,
+			Log:    logger,
 		})
 
+		hc := http.Client{} // some custom HTTP client
+
+		rest := polygonRest.NewWithClient("KsgzThK2sJ2GY3NVGyt2GSsKuWRpoSPp", &hc)
+
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println("连接失败poly")
+			logger.Fatal(err)
 		}
 
 		// connect to the server
 		if err := c.Connect(); err != nil {
-			log.Println(err)
+			fmt.Println("连接失败poly2")
+			logger.Println(err)
 			return
 		}
 
 		defer c.Close()
+
 		var markets []Market
 
 		mysqlDb.Table("markets").Where("category_id = ?", category.Id).Find(&markets)
+
+		if len(markets) == 0 {
+			continue
+		}
 
 		tickers := make([]string, len(markets))
 
 		//循环导出交易对
 		for _, item := range markets {
+			//判断交易对是否存在 如果不存在则创建
+			names, _ := db.ListCollectionNames(context.Background(), nil)
+
+			has := false
+
+			for _, name := range names {
+				if name == item.Symbol {
+					has = true
+					break
+				}
+			}
+
+			if !has {
+				db.CreateCollection(context.TODO(), item.Symbol)
+
+				//获取半年行情
+				limit := 50000
+				order := restModels.Asc
+				params := restModels.ListAggsParams{
+					Ticker:     "X:ETHUSD",
+					From:       restModels.Millis(time.Now().AddDate(0, -6, 0)),
+					To:         restModels.Millis(time.Now()),
+					Order:      &order,
+					Limit:      &limit,
+					Timespan:   restModels.Minute,
+					Multiplier: 1,
+				}
+				iter := rest.AggsClient.ListAggs(context.TODO(), &params)
+				logger.Info("开始导出" + item.Symbol)
+				for iter.Next() {
+					out := iter.Item()
+					//保存到mongoDB中
+					collection := db.Collection(item.Symbol)
+
+					//根据Symbol和Timestamp判断是否存在
+					var result marketData
+
+					//将timestamp转换为int64
+					timestamp := time.Time(out.Timestamp).Unix() * 1000
+
+					collection.FindOne(context.TODO(), marketData{Symbol: item.Symbol, Timestamp: timestamp}).Decode(&result)
+					//如果存在则不保存
+					if result.Symbol == "" {
+						//转换成marketData
+						var data = marketData{
+							Open:      out.Open,
+							High:      out.High,
+							Low:       out.Low,
+							Close:     out.Close,
+							Symbol:    item.Symbol,
+							Timestamp: timestamp,
+							Volume:    out.Volume,
+						}
+						collection.InsertOne(context.TODO(), data)
+					}
+					if !iter.Next() {
+						logger.Info("导出完成" + item.Symbol)
+					}
+				}
+				if iter.Err() != nil {
+					logger.Error(iter.Err())
+				}
+			}
+
 			tickers = append(tickers, item.Symbol)
 		}
 
-		//使用协程来分批次获取数据
-		go func() {
-			if err := c.Subscribe(topic, tickers...); err != nil {
-				log.Fatal(err)
-			}
+		if err := c.Subscribe(topic, tickers...); err != nil {
+			logger.Fatal(err)
+		}
 
-			for {
-				select {
-				case err := <-c.Error():
-					log.Fatal(err)
-				case out, more := <-c.Output():
-					if !more {
-						return
-					}
-					switch out.(type) {
-					case models.EquityAgg:
-						out := out.(models.EquityAgg)
-						collectionName := out.Symbol
-						//保存到mongoDB中
-						collection = db.Collection(collectionName)
+		for {
+			select {
+			case err := <-c.Error():
+				logger.Fatal(err)
+			case out, more := <-c.Output():
+				if !more {
+					return
+				}
+				switch out.(type) {
+				case models.CurrencyAgg:
+					out := out.(models.CurrencyAgg)
+					collectionName := out.Pair
+					//保存到mongoDB中
+					collection := db.Collection(collectionName)
 
-						//根据Symbol和Timestamp判断是否存在
-						var result marketData
-						collection.FindOne(context.Background(), marketData{Symbol: out.Symbol, Timestamp: out.EndTimestamp}).Decode(&result)
-						//如果存在则不保存
-						if result.Symbol == "" {
-							//转换成marketData
-							var data = marketData{
-								Open:      out.Open,
-								High:      out.High,
-								Low:       out.Low,
-								Close:     out.Close,
-								Symbol:    out.Symbol,
-								Timestamp: out.EndTimestamp,
-								Volume:    out.Volume,
-							}
-							collection.InsertOne(context.Background(), data)
-							//根据秒级数据生成分钟级数据
-							collection.Aggregate(context.Background(), mongo.Pipeline{})
-
+					//根据Symbol和Timestamp判断是否存在
+					var result marketData
+					collection.FindOne(context.Background(), marketData{Symbol: out.Pair, Timestamp: out.EndTimestamp}).Decode(&result)
+					//如果存在则不保存
+					if result.Symbol == "" {
+						//转换成marketData
+						var data = marketData{
+							Open:      out.Open,
+							High:      out.High,
+							Low:       out.Low,
+							Close:     out.Close,
+							Symbol:    out.Pair,
+							Timestamp: out.EndTimestamp,
+							Volume:    out.Volume,
 						}
+						collection.InsertOne(context.Background(), data)
 					}
 				}
 			}
-		}()
+		}
+
 	}
 
-	log.Fatal(app.Listen(":3000"))
+	logger.Fatal(app.Listen(":3000"))
 
 }
